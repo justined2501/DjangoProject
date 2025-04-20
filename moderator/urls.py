1ee573f13d4520b0107f0a9a55516ce1b4598232
vitaliikucherenko <vitalii.kucherenko@raccoongang.com>
2025-04-20 15:46:21 +0300

    rm apps moderator and worker

--- moderator/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "moderator"
-urlpatterns = [
-    path('', views.moderator, name="user_detail"),
-]
--- worker/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile,Auto,Sales
-admin.site.register(UserProfile)
-admin.site.register(Auto)
-admin.site.register(Sales)
